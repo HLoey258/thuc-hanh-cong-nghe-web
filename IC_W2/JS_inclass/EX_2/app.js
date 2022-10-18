a447dd2bf99eb1bcb5cd1548126162f894adf893
HLOEY <70062580+HLoey258@users.noreply.github.com>
2022-10-18 20:01:45 +0700

    update app.js

--- IC_W2/JS_inclass/EX_2/app.js
+++ IC_W2/JS_inclass/EX_2/app.js
@@ -2,9 +2,6 @@ const $ = document.querySelector.bind(document);
 const btn = $('.btn');
 btn.onclick = () => {
     btn.classList.toggle('clicked')
-    if (btn.classList == 'btn clicked') {
-        btn.innerText = "Click again"
-    } else {
-        btn.innerText = "Click me"
-    }
+    
+    (btn.classList == 'btn clicked') ?  btn.innerText = "Click again" : btn.innerText = "Click me"
 }
